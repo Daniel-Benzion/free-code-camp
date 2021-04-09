d79eec563750138842f322c1ae4ac5ba67a736b6
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-04-09 17:24:15 -0400

    where-do-i-belong

---
+++ javascript-algorithms-and-data-structures/basic-algorithm-scripting/where-do-i-belong.js
@@ -0,0 +1,7 @@
+function getIndexToIns(arr, num) {
+    arr.sort(function(a, b) { return a - b;});
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= num) return i;
+    }
+    return arr.length;
+}
