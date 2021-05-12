4e3137b6d6be84a1db9393bc0f4f5c140ca1a760
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-05-12 12:00:57 -0400

    implement-map-on-a-prototype

---
+++ javascript-algorithms-and-data-structures/functional-programming/implement-map-on-a-prototype.js
@@ -0,0 +1,12 @@
+// the global Array
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+    const newArray = [];
+    this.forEach(a => newArray.push(callback(a)));
+    return newArray;
+};
+
+const new_s = s.myMap(function (item) {
+    return item * 2;
+});
