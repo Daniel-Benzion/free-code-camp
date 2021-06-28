bcef761aaf8e1d6c8f92ed9845b581e9eeeb1fe3
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-06-27 22:40:17 -0400

    dna-pairing

---
+++ javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/dna-pairing.js
@@ -0,0 +1,12 @@
+function pairElement(str) {
+    const pairs = {
+      A: "T",
+      T: "A",
+      C: "G",
+      G: "C"
+    };
+    let arr = str.split("");
+    return arr.map(x => [x, pairs[x]]);
+  }
+  
+  pairElement("GCG");
